f7dbcfe5ebc96fa4118c5ea243c5038c08a29941
Anton ALLIOT <anton.alliot@live.se>
2014-11-15 16:41:53 +0100

    aa

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,41 @@
 /*   By: aalliot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 12:43:29 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/14 17:19:06 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/15 16:41:27 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "get_next_line.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
 
-int			get_next_line(int fd, char **line)
+int					get_next_line(int fd, char **line)
 {
-	
+	static char		*mem;
+	char			*str;
+	char			buff[BUFF_SIZE + 1];
+
+	if (!fd)
+		return (0);
+	while (ft_strchr(str, '\n'))
+	{
+		read(fd, &buff, BUFF_SIZE);
+		str = ft_strjoin(str, buff);
+	}
+	*line = ft_strsub(str, 0, ft_strchr(str, '\n') - str - 1);	`
+//	read(fd, &buff, BUFF_SIZE);
+}
+
+int			main()
+{
+	char	*str;
+	char	**line;
+	int		c;
+	int		fd;
+
+	fd = open("static.c", O_RDONLY);
+	while (c++ < 10)
+		get_next_line(fd, line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: aalliot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 12:44:13 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/13 14:33:36 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/15 14:51:12 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE = 32
+# define BUFF_SIZE 42
 
 # include <unistd.h>
 
