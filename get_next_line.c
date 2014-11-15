f0accd3c1f515ed6d107633dd0205639ce8f48d6
Anton ALLIOT <anton.alliot@live.se>
2014-11-15 19:37:45 +0100

    modifs

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aalliot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 12:43:29 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/15 16:41:27 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/15 19:36:53 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,37 @@ int					get_next_line(int fd, char **line)
 {
 	static char		*mem;
 	char			*str;
-	char			buff[BUFF_SIZE + 1];
+	char			*buff;
+	int				i;
 
-	if (!fd)
-		return (0);
-	while (ft_strchr(str, '\n'))
+	buff = ft_strnew(BUFF_SIZE);
+	if (!fd || BUFF_SIZE <= 0)
+		return (-1);
+	if (mem)
+		str = ft_strdup(mem);
+	while (!ft_strchr(str, '\n'))
 	{
-		read(fd, &buff, BUFF_SIZE);
+		if (!read(fd, &buff, BUFF_SIZE))
+		{
+			i = 0;
+			break;
+		}
 		str = ft_strjoin(str, buff);
 	}
-	*line = ft_strsub(str, 0, ft_strchr(str, '\n') - str - 1);	`
-//	read(fd, &buff, BUFF_SIZE);
+	*line = ft_strsub(str, 0, ft_strchr(str, '\n') - str - 1);
+	mem = ft_strsub(str, ft_strchr(str, '\n') - str + 1, \
+			ft_strlen(ft_strchr(str, '\n')));
+	return(i == 0 ? 0 : 1);
 }
 
 int			main()
 {
 	char	*str;
 	char	**line;
-	int		c;
 	int		fd;
 
 	fd = open("static.c", O_RDONLY);
-	while (c++ < 10)
-		get_next_line(fd, line);
+	while (get_next_line(fd, line))
+		printf("%s\n", line);
+	return (0);
 }
