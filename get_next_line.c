bccf70dba0e0d8ee05b1d0cefcc046043ac3fb4d
Anton ALLIOT <anton.alliot@live.se>
2014-11-17 16:25:35 +0100

    Final sans bonus

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,19 @@
 /*   By: aalliot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 12:43:29 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/17 15:39:09 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/17 16:22:28 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "get_next_line.h"
-#include <stdlib.h>
-#include <fcntl.h>
-#include <stdio.h>
 
 int					get_next_line(int fd, char **line)
 {
 	static char		*mem;
 	char			*str;
-	char			*buff;
+	char			buff[BUFF_SIZE + 1];
 	int				ret;
 
-	buff = ft_strnew(BUFF_SIZE + 1);
 	if (!fd || BUFF_SIZE <= 0)
 		return (-1);
 	if (mem)
@@ -31,7 +26,7 @@ int					get_next_line(int fd, char **line)
 	while (!ft_strchr(str, '\n'))
 	{
 		if (!(ret = read(fd, buff, BUFF_SIZE)))
-			break;
+			break ;
 		buff[ret] = 0;
 		str = ft_strjoin(str, buff);
 	}
@@ -40,18 +35,4 @@ int					get_next_line(int fd, char **line)
 			ft_strlen(ft_strchr(str, '\n')));
 	free(str);
 	return (ret == 0 ? 0 : 1);
-}
-
-int			main()
-{
-	char	*str;
-	char	*line;
-	int		fd;
-
-	fd = open("static.c", O_RDONLY);
-	while (get_next_line(fd, &line))
-	{
-		printf("%s\n", line);
-	}
-	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,19 @@
 /*   By: aalliot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 12:44:13 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/15 14:51:12 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/17 16:08:58 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 42
+# define BUFF_SIZE 10
 
+# include "libft.h"
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
 
 int			get_next_line(int fd, char **line);
 
