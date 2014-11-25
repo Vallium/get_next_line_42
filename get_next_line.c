9f1bff5bc73141eed84c9b92dc43659e5bbb08e4
Alliot Anton <Vallium@MacBook-Pro-de-Alliot.local>
2014-11-25 10:50:37 +0100

    modifs

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aalliot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 12:43:29 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/21 17:40:24 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/23 17:54:06 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int					get_next_line(int fd, char **line)
 	static char		*mem;
 	char			*str;
 	char			buff[BUFF_SIZE + 1];
-	int				ret;
+	int			ret;
 
 	if (!line || !*line || !fd || BUFF_SIZE <= 0)
 		return (-1);
 	if (mem)
-		str = ft_strdup(mem);
+	str = ft_strdup(mem);
 	while (!(ft_strchr(str, '\n')))
 	{
 		if (!(ret = read(fd, buff, BUFF_SIZE)))
@@ -36,5 +36,7 @@ int					get_next_line(int fd, char **line)
 	mem = ft_strsub(str, ft_strchr(str, '\n') - str + 1, \
 			ft_strlen(ft_strchr(str, '\n')));
 	free(str);
+	ft_putnbr(ret);
+	ft_putstr(" Alive ");
 	return (ret == 0 ? 0 : 1);
 }
