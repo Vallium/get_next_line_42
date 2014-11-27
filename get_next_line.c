184f47af2a15de44453d5eab379439b22b20c7b5
Anton ALLIOT <anton.alliot@live.se>
2014-11-27 20:21:01 +0100

    modifs

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 12:43:29 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/27 17:05:54 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/27 20:09:29 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,23 @@
 int					get_next_line(int fd, char **line)
 {
 	static char		*mem;
-	char			*str;
-	char			buff[BUFF_SIZE + 1];
+	char			buff[BUFF_SIZE];
 	int				ret;
 
-	if (fcntl(fd, F_GETFL) == -1 || !line || BUFF_SIZE <= 0)
+	if (fcntl(fd, F_GETFL) == -1 || BUFF_SIZE <= 0 || !line)
 		return (-1);
-	if (mem)
-		str = ft_strdup(mem);
-	while (!(ft_strchr(str, '\n')))
+	if (!mem)
+		mem = ft_strnew(0);
+	while (!(ft_strchr(mem, '\n')) && (ret = read(fd, buff, BUFF_SIZE)))
 	{
-		if (!(ret = read(fd, buff, BUFF_SIZE)))
-			break ;
 		buff[ret] = 0;
-		str = ft_strjoin(str, buff);
+		mem = ft_strjoin(mem, buff);
 	}
-	if (ft_strchr(str, '\n') || ((*line = ft_strdup(str)) && 0))
-		*line = ft_strsub(str, 0, ft_strchr(str, '\n') - str + 1);
+	if (ft_strchr(mem, '\n') || ((*line = ft_strdup(mem)) && 0))
+		*line = ft_strsub(mem, 0, ft_strchr(mem, '\n') - mem + 1);
 	if (ret)
 		line[0][ft_strlen(*line) - 1] = 0;
-	mem = ft_strsub(str, ft_strchr(str, '\n') - str + 1, \
-			ft_strlen(ft_strchr(str, '\n')));
-	free(str);
+	mem = ft_strsub(mem, ft_strchr(mem, '\n') - mem + 1, \
+			ft_strlen(ft_strchr(mem, '\n')));
 	return (ret == 0 ? 0 : 1);
 }
