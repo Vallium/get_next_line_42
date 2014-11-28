f7de4c05c29ae7c7a29741679c0d7e69ea09efd8
Anton ALLIOT <anton.alliot@live.se>
2014-11-28 14:24:31 +0100

    Final

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: aalliot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 12:44:13 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/28 13:58:07 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/28 14:18:38 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 8
 
-# include "libft/libft.h"
+# include "libft/includes/libft.h"
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
